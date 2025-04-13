22b92251e5860399fa78ef8e1a90a8a348255a45
Ramadhanu <rzidinc@Ramadhanus-MacBook-Pro.local>
2025-04-13 14:00:53 +0700

    chore: move deps order in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 // Deps
+import { useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router";
 
 // Pages
 import Home from "./pages/home";
 import PokemonDetail from "./pages/detail";
-import { useEffect } from "react";
 
 const App = () => {
   const basename = import.meta.env["VITE_APP_BASE_PATH"] || undefined;

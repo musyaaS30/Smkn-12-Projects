a8a3d326de503424cf525452ec42290dfe5c3069
Ramadhanu <rzidinc@Ramadhanus-MacBook-Pro.local>
2025-04-13 14:00:26 +0700

    chore: change setting of basepath and routing file

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+// Deps
+import { BrowserRouter, Routes, Route } from "react-router";
+
+// Pages
+import Home from "./pages/home";
+import PokemonDetail from "./pages/detail";
+import { useEffect } from "react";
+
+const App = () => {
+  const basename = import.meta.env["VITE_APP_BASE_PATH"] || undefined;
+
+  useEffect(() => {
+    console.log(basename);
+  }, [basename]);
+
+  return (
+    <BrowserRouter basename={basename}>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/:id" element={<PokemonDetail />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 // Deps
 import { Provider } from "react-redux";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router";
 
 // Store
 import { store } from "../store/app";
@@ -9,21 +8,14 @@ import { store } from "../store/app";
 // CSS
 import "./index.css";
 
-// Pages
-import Home from "./pages/home";
-import PokemonDetail from "./pages/detail";
+// Main Module
+import App from "./App";
 
 // Constants
 const root = document.getElementById("root");
-const basename = import.meta.env["VITE_APP_BASE_PATH"] || undefined;
 
 ReactDOM.createRoot((root || <></>) as HTMLElement).render(
   <Provider store={store}>
-    <BrowserRouter basename={basename}>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/:id" element={<PokemonDetail />} />
-      </Routes>
-    </BrowserRouter>
+    <App />
   </Provider>
 );
